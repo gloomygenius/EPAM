5213bfdbf94447d770f8e6f39c67f81a96402dce
Vasiliy Bobkov <vasily-bobkow@yandex.ru>
2016-09-07 15:11:32 +0300

    Реализован класс Writing

--- oop/src/main/java/com/epam/homework/oop/t03/Stationery.java
+++ oop/src/main/java/com/epam/homework/oop/t03/Stationery.java
@@ -1,4 +1,27 @@
 package com.epam.homework.oop.t03;
 
 public class Stationery {
+    private String brand;
+    private double price;
+
+    public Stationery(String brand, double price) {
+        this.brand = brand;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 }
--- oop/src/main/java/com/epam/homework/oop/t03/Writing.java
+++ oop/src/main/java/com/epam/homework/oop/t03/Writing.java
@@ -1,4 +1,26 @@
 package com.epam.homework.oop.t03;
 
 public class Writing {
+
+    enum typeOfWritingEnum {PENCIL, PEN}
+
+    private String color;
+    private typeOfWritingEnum typeOfWriting;
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public typeOfWritingEnum getTypeOfWriting() {
+        return typeOfWriting;
+    }
+
+    public void setTypeOfWriting(typeOfWritingEnum typeOfWriting) {
+        this.typeOfWriting = typeOfWriting;
+    }
+
 }
