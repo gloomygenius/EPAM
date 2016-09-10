e94201c3bf1662519399230dd115911fe428a48d
Vasiliy Bobkov <vasily-bobkow@yandex.ru>
2016-09-10 17:46:12 +0300

    Модифицирован тест для Stationary

--- oop/src/test/java/com/epam/homework/oop/t03/StationeryTest.java
+++ oop/src/test/java/com/epam/homework/oop/t03/StationeryTest.java
@@ -8,16 +8,21 @@ import static org.junit.Assert.*;
 public class StationeryTest {
 
 
-    Stationery stationary1 = new Writing("Bic",45.0);
-    Stationery stationary2 = new OfficeTool("Parker",135.0);
-    Stationery stationary3 = new Drawing("DALER ROWNEY",123.0);
+    Stationery stationary1 = new Writing("Bic", 45.0);
+    Stationery stationary2 = new OfficeTool("Parker", 135.0);
+    Stationery stationary3 = new Drawing("DALER ROWNEY", 123.0);
 
     @Test
-    public void testFields(){
-        assertThat(stationary1.getBrand(),is("Bic"));
-        assertThat(stationary2.getBrand(),is("Parker"));
-        assertThat(stationary3.getBrand(),is("DALER ROWNEY"));
-
+    public void testBrand() {
+        assertThat(stationary1.getBrand(), is("Bic"));
+        assertThat(stationary2.getBrand(), is("Parker"));
+        assertThat(stationary3.getBrand(), is("DALER ROWNEY"));
     }
 
+    @Test
+    public void testPrice() {
+        assertThat(stationary1.getPrice(), is(45.0));
+        assertThat(stationary2.getPrice(), is(135.0));
+        assertThat(stationary3.getPrice(), is(123.0));
+    }
 }
