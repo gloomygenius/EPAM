6880de00cf59b96fe7786e1e382a52fed64d1f29
Vasiliy Bobkov <vasily-bobkow@yandex.ru>
2016-09-16 10:38:12 +0300

    Доделана сортировка по двум параметрам в t04

---
+++ oop/src/main/java/com/epam/homework/oop/t04/CompareByBrandAndPrice.java
@@ -0,0 +1,19 @@
+package com.epam.homework.oop.t04;
+
+import com.epam.homework.oop.t03.Stationery;
+
+import java.util.Comparator;
+
+public class CompareByBrandAndPrice implements Comparator<Stationery>{
+    @Override
+    public int compare(Stationery obj1, Stationery obj2){
+        String brand1=obj1.getBrand();
+        String brand2=obj2.getBrand();
+        if (brand1.compareTo(brand2)==0) {
+            if (obj1.getPrice()>obj2.getPrice()) return 1;
+            if (obj1.getPrice()==obj2.getPrice()) return 0;
+            else return -1;
+        }
+        return brand1.compareTo(brand2);
+    }
+}
--- oop/src/test/java/com/epam/homework/oop/t04/CompareTest.java
+++ oop/src/test/java/com/epam/homework/oop/t04/CompareTest.java
@@ -10,14 +10,16 @@ import static org.junit.Assert.*;
 
 public class CompareTest {
     Stationery[] stationaries = new Stationery[5];
-
-    @Test
-    public void testSortByBrand() {
+    {
         stationaries[0] = new OfficeTool("Parker", 135.0);
         stationaries[1] = new Writing("Bic", 45.0);
-        stationaries[2] = new Drawing("DALER ROWNEY", 123.0);
+        stationaries[2] = new Drawing("DALER ROWNEY", 163.0);
         stationaries[3] = new Drawing("Delay", 113.0);
         stationaries[4] = new Drawing("DALER ROWNEY", 153.0);
+    }
+    @Test
+    public void testSortByBrand() {
+
         Arrays.sort(stationaries, new CompareByBrand());
         assertThat(stationaries[0].getBrand(), is("Bic"));
         assertThat(stationaries[1].getBrand(), is("DALER ROWNEY"));
@@ -28,16 +30,22 @@ public class CompareTest {
 
     @Test
     public void testSortByPrice(){
-        stationaries[0] = new OfficeTool("Parker", 135.0);
-        stationaries[1] = new Writing("Bic", 45.0);
-        stationaries[2] = new Drawing("DALER ROWNEY", 123.0);
-        stationaries[3] = new Drawing("Delay", 113.0);
-        stationaries[4] = new Drawing("DALER ROWNEY", 153.0);
         Arrays.sort(stationaries, new CompareByPrice());
         assertThat(stationaries[0].getPrice(), is(45.0));
         assertThat(stationaries[1].getPrice(), is(113.0));
         assertThat(stationaries[2].getPrice(), is(123.0));
         assertThat(stationaries[3].getPrice(), is(135.0));
         assertThat(stationaries[4].getPrice(), is(153.0));
+    }
+    @Test
+    public void testSortByBrandAndPrice(){
+        Arrays.sort(stationaries, new CompareByBrandAndPrice());
+        assertThat(stationaries[0].getBrand(), is("Bic"));
+        assertThat(stationaries[1].getBrand(), is("DALER ROWNEY"));
+        assertThat(stationaries[1].getPrice(), is(153.0));
+        assertThat(stationaries[2].getBrand(), is("DALER ROWNEY"));
+        assertThat(stationaries[2].getPrice(), is(163.0));
+        assertThat(stationaries[3].getBrand(), is("Delay"));
+        assertThat(stationaries[4].getBrand(), is("Parker"));
     }
 }
